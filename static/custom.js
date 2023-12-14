0aaebe41b04c273f23b36d064c97dca734642520
fg-research-dev <tech@fg-research.com>
2023-12-14 17:29:13 +0100

    update website

--- static/custom.js
+++ static/custom.js
@@ -1,9 +1,7 @@
-
 window.setInterval(function(){
     console.log(window.localStorage.getItem('darkMode'));
     var f = document.getElementById('aws-marketplace-logo');
     if(window.localStorage.getItem('darkMode') == 'light'){
-
         f.childNodes[1].href = 'https://aws.amazon.com/marketplace/seller-profile?id=seller-nkd47o2qbdvb2'
         f.childNodes[1].childNodes[0].src = '_static/AWSMP_NewLogo_RGB_BLK.png'
     }else{
