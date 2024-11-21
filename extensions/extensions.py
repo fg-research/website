717b89b0f145d89138530c9cce77cb03bec1298f
fg-research-dev <tech@fg-research.com>
2024-11-21 12:45:17 +0100

    update website

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -12,6 +12,15 @@ def google_analytics(app, pagename, templatename, context, doctree):
       gtag('config', 'G-2L0F07XRQM');
     </script>
     """
+    metatags += """
+    <script type="text/javascript">
+    var _iub = _iub || [];
+    _iub.csConfiguration = {"siteId":3844372,"cookiePolicyId":49690203,"lang":"en","storage":{"useSiteId":true}};
+    </script>
+    <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3844372.js"></script>
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+    """
     context['metatags'] = metatags
 
 
