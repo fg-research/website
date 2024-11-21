c25eaf0681c32f796932acd8d6902e1d6be8a46a
fg-research-dev <tech@fg-research.com>
2024-11-21 16:47:16 +0100

    update website

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -15,52 +15,7 @@ def google_analytics(app, pagename, templatename, context, doctree):
     metatags += """
     <script type="text/javascript">
     var _iub = _iub || [];
-    _iub.csConfiguration = {
-        "floatingPreferencesButtonDisplay": false,
-        "askConsentAtCookiePolicyUpdate":true,
-        "countryDetection":true,
-        "enableFadp":true,
-        "enableLgpd":true,
-        "enableUspr":true,
-        "lang":"en",
-        "lgpdAppliesGlobally":false,
-        "logViaAlert":true,
-        "perPurposeConsent":true,
-        "preferenceCookie":{"expireAfter":1},
-        "siteId":3844372,
-        "storage":{"useSiteId":true},
-        "cookiePolicyId":49690203,
-        "i18n":{"en":{"banner":{
-            "dynamic":{
-            "body": "fg-research.com and selected third parties use cookies or similar technologies for technical purposes and, with your consent, for functionality, experience, measurement and marketing (personalized ads). Use the “Accept” button to consent. Use the “Reject” button to continue without accepting."}}}},
-            "banner":{
-            "acceptButtonCaptionColor":"#0F172A",
-            "acceptButtonColor":"#D3D4D5,
-            "rejectButtonCaptionColor":"#0F172A",
-            "rejectButtonColor":"#D3D4D5",
-            "customizeButtonCaptionColor":"#ffffff",
-            "customizeButtonColor":"#64748B",
-            "customizeButtonDisplay":true,
-            "rejectButtonDisplay":true,
-            "acceptButtonDisplay":true,
-            "backgroundColor":"#FFFFFF",
-            "closeButtonDisplay":false,
-            "explicitWithdrawal":true,
-            "fontSizeBody":"12px",
-            "listPurposes":true,
-            "linksColor":"#0F172A",
-            "ownerName":"fg-research.com",
-            "position":"float-bottom-center",
-            "showPurposesToggles":false,
-            "showTitle":false,
-            "showTotalNumberOfProviders":true,
-            "slideDown":false,
-            "textColor":"#0F172A",
-            "acceptButtonCaption":"Accept",
-            "customizeButtonCaption":"Learn more",
-            "rejectButtonCaption":"Reject"
-            }
-        };
+    _iub.csConfiguration = {"floatingPreferencesButtonDisplay": false, "askConsentAtCookiePolicyUpdate":true,"countryDetection":true,"emailMarketing":{"styles":{"backgroundColor":"#FFFFFF","buttonsBackgroundColor":"#D3D4D5","buttonsTextColor":"#0F172A","footerBackgroundColor":"#64748B","footerTextColor":"#FFFFFF","textColor":"#0F172A"}},"enableFadp":true,"enableLgpd":true,"enableUspr":true,"lang":"en","lgpdAppliesGlobally":false,"logViaAlert":true,"perPurposeConsent":true,"preferenceCookie":{"expireAfter":180},"siteId":3844372,"storage":{"useSiteId":true},"cookiePolicyId":49690203,"i18n":{"en":{"banner":{"dynamic":{"body":"fg-research.com and selected third parties use cookies or similar technologies for technical purposes and, with your consent, for functionality, experience, measurement and marketing (personalized ads). Use the “Accept” button to consent. Use the “Reject” button to continue without accepting."}}}},"banner":{"acceptButtonCaptionColor":"#0F172A","acceptButtonColor":"#D3D4D5","acceptButtonDisplay":true,"backgroundColor":"#FFFFFF","closeButtonDisplay":false,"customizeButtonColor":"#64748B","customizeButtonDisplay":true,"explicitWithdrawal":true,"fontSizeBody":"12px","listPurposes":true,"linksColor":"#0F172A","ownerName":"fg-research.com","position":"float-bottom-center","rejectButtonCaptionColor":"#0F172A","rejectButtonColor":"#D3D4D5","rejectButtonDisplay":true,"showPurposesToggles":false,"showTitle":false,"showTotalNumberOfProviders":true,"slideDown":false,"textColor":"#0F172A","acceptButtonCaption":"Accept","customizeButtonCaption":"Learn more","rejectButtonCaption":"Reject"}};
     </script>
     <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3844372.js"></script>
     <script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
