baf7d47cd56620223add90b52b6ab6bc334c9a8f
fg-research-dev <tech@fg-research.com>
2024-11-21 14:38:00 +0100

    update website

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -12,15 +12,58 @@ def google_analytics(app, pagename, templatename, context, doctree):
       gtag('config', 'G-2L0F07XRQM');
     </script>
     """
-    # metatags += """
-    # <script type="text/javascript">
-    # var _iub = _iub || [];
-    # _iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"countryDetection":true,"emailMarketing":{"styles":{"backgroundColor":"#FFFFFF","buttonsBackgroundColor":"#3B82F6","buttonsTextColor":"#FFFFFF","footerBackgroundColor":"#64748B","footerTextColor":"#FFFFFF","textColor":"#0F172A"}},"enableFadp":true,"enableLgpd":true,"enableUspr":true,"gdprAppliesGlobally":false,"lang":"en","lgpdAppliesGlobally":false,"perPurposeConsent":true,"siteId":3844372,"storage":{"useSiteId":true},"cookiePolicyId":49690203,"banner":{"acceptButtonColor":"#3B82F6","acceptButtonDisplay":true,"backgroundColor":"#FFFFFF","closeButtonDisplay":false,"customizeButtonColor":"#64748B","customizeButtonDisplay":true,"explicitWithdrawal":true,"fontSizeBody":"12px","listPurposes":true,"linksColor":"#0F172A","ownerName":"fg-research.com","position":"float-bottom-center","rejectButtonColor":"#3B82F6","rejectButtonDisplay":true,"showTitle":false,"showTotalNumberOfProviders":true,"textColor":"#0F172A"}};
-    # </script>
-    # <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3844372.js"></script>
-    # <script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
-    # <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
-    # """
+    metatags += """
+    <script type="text/javascript">
+    var _iub = _iub || [];
+    _iub.csConfiguration = {
+    "floatingPreferencesButtonDisplay": false,
+    "askConsentAtCookiePolicyUpdate":true,
+    "countryDetection":true,
+    "emailMarketing":{
+        "styles":{
+            "backgroundColor":"#FFFFFF",
+            "buttonsBackgroundColor":"#3B82F6",
+            "buttonsTextColor":"#FFFFFF",
+            "footerBackgroundColor":"#64748B",
+            "footerTextColor":"#FFFFFF",
+            "textColor":"#0F172A"
+        }
+    },
+    "enableFadp":true,
+    "enableLgpd":true,
+    "enableUspr":true,
+    "gdprAppliesGlobally":false,
+    "lang":"en",
+    "lgpdAppliesGlobally":false,
+    "perPurposeConsent":true,
+    "siteId":3844372,
+    "storage":{"useSiteId":true},
+    "cookiePolicyId":49690203,
+    "banner":{
+        "acceptButtonColor":"#3B82F6",
+        "acceptButtonDisplay":true,
+        "backgroundColor":"#FFFFFF",
+        "closeButtonDisplay":false,
+        "customizeButtonColor":"#64748B",
+        "customizeButtonDisplay":true,
+        "explicitWithdrawal":true,
+        "fontSizeBody":"12px",
+        "listPurposes":true,
+        "linksColor":"#0F172A",
+        "ownerName":"fg-research.com",
+        "position":"float-bottom-center",
+        "rejectButtonColor":"#3B82F6",
+        "rejectButtonDisplay":true,
+        "showTitle":false,
+        "showTotalNumberOfProviders":true,
+        "textColor":"#0F172A"
+        }
+    };
+    </script>
+    <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3844372.js"></script>
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+    """
     context['metatags'] = metatags
 
 
